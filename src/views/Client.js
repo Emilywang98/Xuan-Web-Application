1ee02322d69549c9779a5fb1bb3440a2431a3296
Oscar <oscarla5747@gmail.com>
2021-09-02 13:25:53 -0700

    bbq changes

--- src/views/Client.js
+++ src/views/Client.js
@@ -40,6 +40,7 @@ import Calendar from "react-calendar";
 
 import Week from "../components/Schedule/Week"
 import ExerciseModal from "../components/Modals/ExerciseModal"
+import HabitCard from "components/Habit/HabitCard";
 
 function Workout() {
   const [firstDate, setFirstDate] = useState(new Date());
@@ -339,107 +340,7 @@ function Workout() {
                 </Form>
               </CardBody>
             </Card> */}
-            <Card className="card-user">
-              <CardHeader>
-                <CardTitle tag="h5">Add Habit</CardTitle>
-              </CardHeader>
-              <CardBody>
-                <Form>
-                  <Row>
-                    <Col className="pr-1" md="5">
-                      <FormGroup check>
-                        <label>Select Habit Days</label>
-                        <Input
-                          type="select"
-                          name="selectMulti"
-                          id="exampleSelectMulti"
-                          multiple
-                        >
-                          <option>Sunday</option>
-                          <option>Monday</option>
-                          <option>Tuesday</option>
-                          <option>Wednesday</option>
-                          <option>Thursday</option>
-                          <option>Friday</option>
-                          <option>Saturday</option>
-                        </Input>
-                      </FormGroup>
-                      <FormGroup>
-                        <label>Enter Question</label>
-                        <Input
-                          defaultValue=""
-                          // disabled
-                          placeholder="Question"
-                          type="text"
-                        />
-                      </FormGroup>
-                      <Button
-                        className="btn-round"
-                        color="primary"
-                        type="submit"
-                      >
-                        Add Habit
-                      </Button>
-                    </Col>
-                    <Col>
-                      <ListGroup>
-                        <ListGroupItem>
-                          {/* <ListGroupItemHeading>
-                            List group item heading
-                          </ListGroupItemHeading>
-                          <ListGroupItemText>
-                            Donec id elit non mi porta gravida at eget metus.
-                            Maecenas sed diam eget risus varius blandit.
-                          </ListGroupItemText> */}
-                          <h6>Sunday</h6>
-                          <ListGroup>
-                            <ListGroupItem>Drink 1L of Water</ListGroupItem>
-                          </ListGroup>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                          <h6>Monday</h6>
-                          <ListGroup>
-                            <ListGroupItem>Drink 1L of Water</ListGroupItem>
-                          </ListGroup>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                          <h6>Tuesday</h6>
-                          <ListGroup>
-                            <ListGroupItem>Drink 1L of Water</ListGroupItem>
-                            <ListGroupItem>Practice DJ-ing</ListGroupItem>
-                          </ListGroup>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                          <h6>Wednesday</h6>
-                          <ListGroup>
-                            <ListGroupItem>Drink 1L of Water</ListGroupItem>
-                          </ListGroup>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                          <h6>Thursday</h6>
-                          <ListGroup>
-                            <ListGroupItem>Drink 1L of Water</ListGroupItem>
-                            <ListGroupItem>Practice DJ-ing</ListGroupItem>
-                          </ListGroup>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                          <h6>Friday</h6>
-                          <ListGroup>
-                            <ListGroupItem>Drink 1L of Water</ListGroupItem>
-                          </ListGroup>
-                        </ListGroupItem>
-                        <ListGroupItem>
-                          <h6>Saturday</h6>
-                          <ListGroup>
-                            <ListGroupItem>Drink 1L of Water</ListGroupItem>
-                          </ListGroup>
-                        </ListGroupItem>
-                      </ListGroup>
-                    </Col>
-                  </Row>
-                </Form>
-              </CardBody>
-            </Card>
+            <HabitCard/>
           </Col>
         </Row>
       </div>
--- src/views/Program.js
+++ src/views/Program.js
@@ -15,109 +15,16 @@ import {
   ListGroupItem,
   ListGroup,
 } from "reactstrap";
+import Week from "../components/Schedule/Week"
 function Program(props) {
 
   return (
     <>
       <div className="content">
         <h5 className="text-primary title">Week 1</h5>
-        <Card>
-          <CardBody>
-            <ListGroup horizontal>
-              <ListGroupItem>
-                Sunday
-                <ListGroup>
-                  <ListGroupItem>
-                    Warm Up
-                    <ListGroup>
-                      <ListGroupItem>Arm Circles</ListGroupItem>
-                      <ListGroupItem>Hip Rotations</ListGroupItem>
-                      <ListGroupItem>Jump Rope</ListGroupItem>
-                      <ListGroupItem>Deep Lunge</ListGroupItem>
-                    </ListGroup>
-                  </ListGroupItem>
-                  <ListGroupItem>
-                    Cardio<ListGroup></ListGroup>
-                  </ListGroupItem>
-                  <ListGroupItem>
-                    Lower Body<ListGroup></ListGroup>
-                  </ListGroupItem>
-                  <ListGroupItem>
-                    Cool Down<ListGroup></ListGroup>
-                  </ListGroupItem>
-                </ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Monday
-                <ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Tuesday<ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Wednesday<ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Thursday<ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Friday<ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Saturday<ListGroup></ListGroup>
-              </ListGroupItem>
-            </ListGroup>
-          </CardBody>
-        </Card>
+        <Week/>
         <h5 className="text-primary title">Week 2</h5>
-        <Card>
-          <CardBody>
-            <ListGroup horizontal>
-              <ListGroupItem>
-                Sunday
-                <ListGroup>
-                  <ListGroupItem>
-                    Warm Up
-                    <ListGroup>
-                      <ListGroupItem>Arm Circles</ListGroupItem>
-                      <ListGroupItem>Hip Rotations</ListGroupItem>
-                      <ListGroupItem>Jump Rope</ListGroupItem>
-                      <ListGroupItem>Deep Lunge</ListGroupItem>
-                    </ListGroup>
-                  </ListGroupItem>
-                  <ListGroupItem>
-                    Cardio<ListGroup></ListGroup>
-                  </ListGroupItem>
-                  <ListGroupItem>
-                    Lower Body<ListGroup></ListGroup>
-                  </ListGroupItem>
-                  <ListGroupItem>
-                    Cool Down<ListGroup></ListGroup>
-                  </ListGroupItem>
-                </ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Monday
-                <ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Tuesday<ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Wednesday<ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Thursday<ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Friday<ListGroup></ListGroup>
-              </ListGroupItem>
-              <ListGroupItem>
-                Saturday<ListGroup></ListGroup>
-              </ListGroupItem>
-            </ListGroup>
-          </CardBody>
-        </Card>
+        <Week/>
       </div>
     </>
   );
